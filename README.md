446ab7693a00586247ef61c25b98eb2bbffd5daa
Romashka093 <aa.romashchenko@gmail.com>
2020-03-27 20:29:10 +0200

    initial pack for homework

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # goit-js-hw-08-gallery
+
 Распространение. Всплытие. Делегирование. Throttle and Debounce. IntersectionObserver API
+
+> [workingpage](https://romashka093.github.io/goit-js-hw-08-gallery/src/index.html)
---
+++ src/js/gallery.js
@@ -0,0 +1,40 @@
+"use strick";
+
+import galleryItems from "./../gallery-items.js";
+
+console.log(galleryItems);
+
+const ref = {
+  gallery: document.querySelector(".js-gallery")
+};
+
+console.log("Gallery", ref.gallery);
+// Создание и рендер разметки по массиву данных и предоставленному шаблону.
+let imageItem = "";
+galleryItems.forEach(item => {
+  imageItem = `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+  >
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      data-source=${item.original}
+      alt=${item.description}
+    />
+  </a>
+</li>`;
+  ref.gallery.insertAdjacentHTML("beforeend", imageItem);
+});
+
+// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
+// Открытие модального окна по клику на элементе галереи.
+// Подмена значения атрибута src элемента img.lightbox__image.
+// Закрытие модального окна по клику на кнопку button[data-action="close-modal"].
+// Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того,
+// чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
+
+// Закрытие модального окна по клику на div.lightbox__overlay.
+// Закрытие модального окна по нажатию клавиши ESC.
+// Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
