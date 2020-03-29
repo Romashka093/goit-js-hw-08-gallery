394cf1ca69603a810300b59b42d6b9de254b6b94
Romashka093 <aa.romashchenko@gmail.com>
2020-03-29 22:46:41 +0300

    almost done

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -5,17 +5,21 @@ import galleryItems from "./../gallery-items.js";
 console.log(galleryItems);
 
 const ref = {
-  gallery: document.querySelector(".js-gallery")
+  gallery: document.querySelector(".js-gallery"),
+  closeButton: document.querySelector('button[data-action="close-lightbox"]'),
+  overlay: document.querySelector("div.lightbox__content")
 };
 
-console.log("Gallery", ref.gallery);
+console.log("Gallery:", ref.gallery);
+console.log("Btn:", ref.closeButton);
+console.log("Overlay:", ref.overlay);
 // Создание и рендер разметки по массиву данных и предоставленному шаблону.
 let imageItem = "";
 galleryItems.forEach(item => {
   imageItem = `<li class="gallery__item">
   <a
     class="gallery__link"
-    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+    href=${item.original}
   >
     <img
       class="gallery__image"
@@ -29,12 +33,72 @@ galleryItems.forEach(item => {
 });
 
 // Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
-// Открытие модального окна по клику на элементе галереи.
-// Подмена значения атрибута src элемента img.lightbox__image.
+ref.gallery.addEventListener("click", handeleOpenItem);
+ref.closeButton.addEventListener("click", hangeleCloseButton);
+ref.overlay.addEventListener("click", hangeleCloseOverlay);
+
+function handeleOpenItem(evt) {
+  evt.preventDefault();
+
+  const targetImage = evt.target;
+
+  if (targetImage === evt.currentTarget) {
+    return; // при нажатии на ul - мы выходим
+  }
+
+  // Открытие модального окна по клику на элементе галереи.
+  const open = document.querySelector("div.lightbox");
+  open.classList.add("is-open");
+  // Подмена значения атрибута src элемента img.lightbox__image.
+  const img = document.querySelector(".lightbox__image");
+  img.alt = targetImage.alt;
+  img.src = targetImage.dataset.source;
+  window.addEventListener("keyup", handleEscape);
+  window.addEventListener("keypress", handleScrolling);
+}
 // Закрытие модального окна по клику на кнопку button[data-action="close-modal"].
-// Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того,
-// чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
+function hangeleCloseButton() {
+  const open = document.querySelector("div.lightbox");
+  open.classList.remove("is-open");
+  // Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того,
+  // чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
+  const img = document.querySelector(".lightbox__image");
+  img.alt = "";
+  img.src = "";
+  window.removeEventListener("keyup", handleEscape);
+  window.removeEventListener("keypress", handleScrolling);
+}
 
 // Закрытие модального окна по клику на div.lightbox__overlay.
+function hangeleCloseOverlay(evt) {
+  if (evt.target !== evt.currentTarget) {
+    return;
+  }
+
+  hangeleCloseButton();
+}
 // Закрытие модального окна по нажатию клавиши ESC.
+function handleEscape(evt) {
+  if (evt.code !== "Escape") {
+    return;
+  }
+  hangeleCloseButton();
+}
 // Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
+
+function handleScrolling(evt) {
+  console.dir(evt.keyCode);
+  console.log(evt.target.nodeName);
+  console.log(evt.key);
+  console.dir(evt.code);
+  const item = document.querySelector("li.gallery__item");
+  console.dir(item.nodeName);
+  if (evt.target !== evt.currentTarget) {
+    if (evt.code === "Period") {
+      console.log("Yep!, see next");
+      console.log(item.nextElementSibling);
+    } else if (evt.code === "Comma") {
+      console.log("No! Lets see prev");
+    }
+  }
+}
